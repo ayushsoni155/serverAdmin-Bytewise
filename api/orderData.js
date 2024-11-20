e24742fe605dce7bb32f17f4c0aca845ebc57bf5
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2024-11-20 11:05:03 +0530

    Update orderData.js

--- api/orderData.js
+++ api/orderData.js
@@ -53,7 +53,7 @@ router.options('*', (req, res) => {
 });
 
 // Fetch orders data
-router.get('/ordersData', async (req, res) => {
+router.get('/orderData', async (req, res) => {
   const query = `
     SELECT 
       orders.orderID,
