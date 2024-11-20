c78f8c1bb59be26c3f0de848000b7799e0f1a58e
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2024-11-20 11:08:14 +0530

    Update orderData.js

--- api/orderData.js
+++ api/orderData.js
@@ -15,7 +15,10 @@ const cors = Cors({
 
 // Setup the database connection pool
 const db = mysql.createPool({
-  host:
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // Helper function to run middleware
